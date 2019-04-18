4c3cbd1d89c70f2bbaabeb735c4a58493c34320b
pmmoncada <pml350@nyu.edu>
2019-04-18 17:40:16 -0400

    Map of TGU

---
+++ js/DataTGU.js
@@ -0,0 +1,97 @@
+var shoppingTGU = [
+    {
+      "name": "centro comercial city mall",
+      "lng": 14.0626255,
+      "lat": -87.2202937
+    },
+    {
+      "name": "el dorado",
+      "lng": 14.0997455,
+      "lat": -87.1819216
+    },
+    {
+      "name": "plaza santa monica kennedy",
+      "lng": 14.0639455,
+      "lat": -87.1840084
+    },
+    {
+      "name": "plaza las lomas",
+      "lng": 14.0930393,
+      "lat": -87.1808083
+    },
+    {
+      "name": "hotel honduras maya",
+      "lng": 14.1036738,
+      "lat": -87.1974748
+    },
+    {
+      "name": "multiplaza",
+      "lng": 14.0881644,
+      "lat": -87.1833556
+    },
+    {
+      "name": "la galeria",
+      "lng": 14.0962253,
+      "lat": -87.1864804
+    },
+    {
+      "name": "los arcos mall",
+      "lng": 14.1008354,
+      "lat": -87.1773183
+    },
+    {
+      "name": "centros commercial villas del sol",
+      "lng": 14.0659806,
+      "lat": -87.1730603
+    },
+    {
+      "name": "metro mall",
+      "lng": 14.0722751,
+      "lat": -87.192136
+    },
+    {
+      "name": "central america mall",
+      "lng": 14.0847695,
+      "lat": -87.197373
+    },
+    {
+      "name": "miramontes mall",
+      "lng": 14.0867523,
+      "lat": -87.2009771
+    },
+    {
+      "name": "centro comercial novacentro",
+      "lng": 14.1015169,
+      "lat": -87.186927
+    },
+    {
+      "name": "centro comercial los castaños",
+      "lng": 14.1003001,
+      "lat": -87.1893075
+    },
+    {
+      "name": "plaza santa monica miraflores",
+      "lng": 14.0712839,
+      "lat": -87.219837
+    },
+    {
+      "name": "centro comercial plaza la granja",
+      "lng": 14.0836317,
+      "lat": -87.2089349
+    },
+    {
+      "name": "comercial plaza san carlos",
+      "lng": 14.0766772,
+      "lat": -87.2007709
+    },
+    {
+      "name": "mall cascadas",
+      "lng": 14.0768491,
+      "lat": -87.2004735
+    },
+    {
+      "name": "centro comercial lomas del boulevard",
+      "lng": 14.100596,
+      "lat": -87.1853703
+    }
+]
---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+
+// sets up my mapbox access token so they can track my usage of their basemap services
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';
+
+var map = new mapboxgl.Map({
+  container: 'mapContainer',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-87.197714, 14.071646],
+  zoom: 12.5,
+});
+
+
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+//Add Inmobiliaria Paris to the Map
+map.on('load', function() {
+    map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Eiffel_Tower_Silhouette.svg/128px-Eiffel_Tower_Silhouette.svg.png', function(error, image) {
+        if (error) throw error;
+        map.addImage('tower', image);
+        map.addLayer({
+            "id": "points",
+            "type": "symbol",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [-87.1876945,14.0757734]
+                        }
+                    }]
+                }
+            },
+            "layout": {
+                "icon-image": "tower",
+                "icon-size": 0.20
+            }
+        });
+    });
+});
+
+
+shoppingTGU.forEach(function(DataTGU) {
+  new mapboxgl.Marker({
+    color: "purple"
+  })
+    .setLngLat([DataTGU.lat, DataTGU.lng])
+    .setPopup(new mapboxgl.Popup({ offset: 40 })
+        .setText(`${DataTGU.name}`))
+    .addTo(map);
+})
